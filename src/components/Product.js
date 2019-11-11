32c3d37cd68b9d3148ac131b7f0078eefd752c8e
marybahati <bahatimary745@gmail.com>
2019-11-11 11:33:12 +0300

    changes

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import one from "../img/two.png"
+import one from "../img/packet.png"
 import two from "../img/one.png"
 import three from "../img/three.png"
 class Product extends React.Component {
@@ -10,22 +10,22 @@ class Product extends React.Component {
      <h2 className="pro">Products</h2>
 <div className="prd">
       <div className="pro1">
-      <img src={one} />
-      <h5>For baking,cooking and facial(100g) </h5>
+      <img src={two}/>
+      <h5>For baking,cooking and facial(187.5g) </h5>
       <h5>Ksh.280 </h5>
       <a href="#" className="btn btn-success">Buy Now</a>
       </div>
 
      <div className="pro2">
-      <img src={two} />
-      <h5>For cooking and making juice(200g)</h5>
+      <img src={two}/>
+      <h5>For cooking and making juice(375g)</h5>
       <h5>Ksh.450 </h5>
       <a href="#" className="btn btn-success">Buy Now</a>
       </div>
 
      <div className="pro3">
-      <img src={three} />
-      <h5>For making juice(500g) </h5>
+      <img src={two}/>
+      <h5>For making juice(750g) </h5>
       <h5>Ksh.850 </h5>
       <a href="#" className="btn btn-success">Buy Now</a>
       </div>
--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import bg from "../img/prd.jpg"
+import bg from "../img/glass.jpg"
 import sec from "../img/ses.jpg"
 import thd from "../img/ses2.jpg"
 class Steps extends React.Component {
@@ -11,13 +11,13 @@ class Steps extends React.Component {
                 <div className="step">
                     <div className="steps">
                     <h4>STEPS</h4>
-                    <h6>1.Put 2 glasses of water</h6>
-                    <h6>2.Add 250g of mango powder</h6>
+                    <h6>1.Put 1 glasses of water</h6>
+                    <h6>2.Add 5 tablespoons of mango powder</h6>
                     <h6>3.Stir the juice and ready to drink</h6>  
                 </div> 
 
                 <div className="video">
-                    <img src={bg}/>  
+                    <img src={sec}  width='500px'/>  
                 </div>
                 </div>
                 </div>
@@ -25,8 +25,8 @@ class Steps extends React.Component {
                 <div className="container">
                 <div id="about">
             <div className="video2">
-                <div className="img1"><img src={sec}/></div>
-                <div className="img2"><img src={thd}/></div>
+                <div className="img1"><img src={bg} height='435px' width='520px'/></div>
+                <div className="img2"><img src={thd} width='520px'/></div>
             </div> 
             </div>
             </div>
